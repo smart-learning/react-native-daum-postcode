3fcd4eadc7e1610e5f2b572beb5d2f0077903f44
woohyuk <hyuk.woo@influential.co.kr>
2019-08-29 16:00:23 +0900

    Update index.js
    daum post code 팝업창이 안나오는 현상 , autoload=false 옵션 추가

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const html = `
 		function initOnReady(options) {
     	window.options = options;
 			var s = document.createElement('script');
-			s.type = 'text/javascript'; s.src = 'https://ssl.daumcdn.net/dmaps/map_js_init/postcode.v2.js';
+			s.type = 'text/javascript'; s.src = 'https://ssl.daumcdn.net/dmaps/map_js_init/postcode.v2.js?autoload=false';
 			s.onreadystatechange = callback; s.onload = callback;
 			var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
     }
